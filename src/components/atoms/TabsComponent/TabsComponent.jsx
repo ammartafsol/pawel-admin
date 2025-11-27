40a1e3bceab8797f67bc61101104911cf530a998
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-11-27 20:16:56 +0500

    TabsComponent

--- src/components/atoms/TabsComponent/TabsComponent.jsx
+++ src/components/atoms/TabsComponent/TabsComponent.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import classes from "./TabsComponent.module.css"
 
 export default function TabsComponent() {
+  const [selectedTab, setSelectedTab] = useState('staff');
+
   return (
-    <div>
-      
+    <div className={classes.tabsContainer}>
+      <button
+        className={`${classes.tab} ${selectedTab === 'staff' ? classes.tabActive : classes.tabInactive}`}
+        onClick={() => setSelectedTab('staff')}
+      >
+        Staff Users
+      </button>
+      <button
+        className={`${classes.tab} ${selectedTab === 'client' ? classes.tabActive : classes.tabInactive}`}
+        onClick={() => setSelectedTab('client')}
+      >
+        Client Users
+      </button>
     </div>
   )
 }
