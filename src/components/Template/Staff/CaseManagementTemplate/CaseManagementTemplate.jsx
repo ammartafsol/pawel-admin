9056d9c9ec4b22c6fd67fb7264a9e990c0ebce40
ammartafsol <ammar.js@tafsol.com>
2025-12-08 11:32:40 +0500

    Enhance CaseManagementTemplate with data fetching and loading state
    Added useEffect for data fetching with pagination and search functionality in CaseManagementTemplate. Integrated useAxios for API calls and implemented loading state management. Updated state variables for handling records and search input.

--- src/components/Template/Staff/CaseManagementTemplate/CaseManagementTemplate.jsx
+++ src/components/Template/Staff/CaseManagementTemplate/CaseManagementTemplate.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from "./CaseManagementTemplate.module.css"
 import Wrapper from '@/components/atoms/Wrapper/Wrapper';
 import TableHeader from '@/components/molecules/TableHeader/TableHeader';
@@ -10,13 +10,37 @@ import { Col, Row } from "react-bootstrap";
 import { caseManagementCardsData } from '@/developementContent/Data/dummtData/dummyData';
 import CreateNewCaseModal from '@/components/organisms/Modals/CreateNewCaseModal/CreateNewCaseModal';
 import useDebounce from '@/resources/hooks/useDebounce';
-
+import useAxios from '@/interceptor/axios-functions';
 
 const CaseManagementTemplate = () => {
   const [showCreateNewCaseModal, setShowCreateNewCaseModal] = useState(false);
   const [selectedDropdownValue, setSelectedDropdownValue] = useState(reactActivities[0]);
+  const [loading,setLoading] = useState('');
+  const [page,setPage] = useState(1);
+  const [totalRecords,setTotalRecords] = useState(0);
+  const [data, setData] = useState([]);
   const [search,setSearch] = useState("");
   const debouceSearch = useDebounce(search, 500);
+  const { Get } = useAxios();
+  const getData = async (_page) => {
+    setLoading('loading');
+    const query = {
+      search: debouceSearch,
+      page: _page || page || 1,
+      limit: RECORDS_LIMIT,
+    }
+    const { response } = await Get({ route: `` });
+    if(response) {
+      setTotalRecords(response?.totalRecords);
+      setData(response?.data);
+    }
+    setLoading('');
+    }
+
+    // useEffect(()=>{
+    //   getData(page);
+    // },[debouceSearch,page]);
+
   return (
     <div className='p24'>
       <Wrapper   headerComponent={<TableHeader viewButtonText='Create new case' searchValue={search} onSearchChange={setSearch} onClickViewAll={() => setShowCreateNewCaseModal(true)}  dropdownOptions={reactActivities}  selectedDropdownValue={selectedDropdownValue} setSelectedDropdownValue={setSelectedDropdownValue} title="Case Management" titleIcon={<FaRegFolderClosed color='#D9D9D9' size={20} />} />}>
